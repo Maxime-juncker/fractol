d7398ac09ba31542304984f0e02fdc66ee455b9b
Maxime Juncker <maxime.junck@gmail.com>
2024-12-10 11:25:09 +0100

    hooks
    [feature]: pressing 'q' will quit the window
    [feature]: pressing 'w' will increase the fractal size
    [feature]: pressing 's' will decrease the fractal size

--- src/main.c
+++ src/main.c
@@ -9,6 +9,11 @@ typedef struct s_specs
 	size_t	width;
 	size_t	height;
 	char *title;
+
+	void	*mlx;
+	void	*mlx_win;
+
+	float scale;
 } t_specs;
 
 typedef struct	s_data {
@@ -45,30 +50,19 @@ int	compute_color(int iteration, int max_iteration)
 
 	r = 255 * iteration / max_iteration;
 	g = 255 * iteration / max_iteration;
-	b = 255 * iteration / max_iteration;
+	b = 100;
 	// r = (hue / 255.0) * 255;
     // g = ((255 - hue) / 255.0) * 255;
     // b = 128	;
 	return create_trgb(0, r, g, b);
 }
 
-int main(void)
+
+void	draw_fractal(t_specs *specs)
 {
-	void	*mlx;
-	void	*mlx_win;
 	t_data	img;
-	t_specs	specs;
-	int	threshold = 700;
-	int	scale = 150;
 
-	specs.width = 720;
-	specs.height = 420;
-	specs.title = "HAAAAAA";
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, specs.width, specs.height, specs.title);
-
-	img.img = mlx_new_image(mlx, specs.width, specs.height);
+	img.img = mlx_new_image(specs->mlx, specs->width, specs->height);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
 	float i = 0;
@@ -78,19 +72,21 @@ int main(void)
 	float complex c;
 	float complex z;
 
-	while (j < specs.height)
+	int	threshold = 10;
+
+	while (j < specs->height)
 	{
 		i = 0;
-		while (i < specs.width)
+		while (i < specs->width)
 		{
-			c = ((i - specs.width / 2) / scale) + (((j - specs.height / 2) * I) / scale);
+			c = ((i - specs->width / 2)) + (((j - specs->height / 2) * I));
 			z = 0;
 			iteration = 0;
 			while (iteration < max_iteration)
 			{
 				if (cabsf(z) >= threshold)
 					break;
-				z = z*z + c;
+				z = z*z + c * specs->scale;
 				iteration++;
 			}
 
@@ -100,8 +96,46 @@ int main(void)
 		}
 		j++;
 	}
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(specs->mlx, specs->mlx_win, img.img, 0, 0);
+}
+
+
+int	win_close(int code, t_specs *specs)
+{
+	if (code == 'q')
+		mlx_destroy_window(specs->mlx, specs->mlx_win);
+	if (code == 's')
+	{
+		specs->scale += .001f;
+		draw_fractal(specs);
+	}
+	if (code == 'w')
+	{
+		specs->scale -= .001f;
+		draw_fractal(specs);
+	}
+	return (0);
+}
+
+int main(void)
+{
+
+	t_specs	specs;
+
+	specs.width = 720;
+	specs.height = 420;
+	specs.title = "HAAAAAA";
+
+	specs.scale = .005f;
+
+	specs.mlx = mlx_init();
+	specs.mlx_win = mlx_new_window(specs.mlx, specs.width, specs.height, specs.title);
+
+	mlx_hook(specs.mlx_win, 2, 1L<<0, win_close, &specs); //? 1L<<0 == keypress mask
+
+	draw_fractal(&specs);
+
+	mlx_loop(specs.mlx);
 
 
 }
