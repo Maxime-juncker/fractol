5452cacba0b24bfd8c7083edd7287fbd95d2d4d1
Maxime Juncker <maxime.junck@gmail.com>
2024-12-10 10:06:41 +0100

    init
    [feature]: mlx setup
    [feature]: first mandlebrot
    [to fix]: mandlebrot is not in the right position

---
+++ Makefile
@@ -0,0 +1,84 @@
+NAME = fractol
+CFLAGS = -Wall -Wextra -Werror -g3 $(INCLUDES_D)
+MAKEFLAGS += --no-print-directory
+
+SRC =	main.c
+
+OBJ = $(SRC:.c=.o)
+
+OBJ_D = obj/
+SRCS_D = src/
+BIN_D = bin/
+LOG_D = log/
+INCLUDES_D = -Iincludes/ -Ilibft/includes/
+
+OBJ := $(addprefix $(OBJ_D), $(OBJ))
+SRCS := $(addprefix $(SRCS_D), $(SRCS))
+
+# colors
+RED 	= \033[31m
+GREEN 	= \033[32m
+YELLOW 	= \033[33m
+RESET 	= \033[0m
+BLUE 	= \033[34m
+
+RM = rm -fr
+# ARGS =  
+
+all: $(BIN_D)$(NAME)
+
+$(BIN_D)$(NAME): $(OBJ) $(BIN_D)
+	echo "$(YELLOW)[MAKE]: libft$(RESET)"
+	$(MAKE)  -C libft
+	$(CC) $(CFLAGS) $(OBJ) libft/bin/libft.a -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -o $(BIN_D)$(NAME)
+	echo "$(YELLOW)[CREATING EXE]: $@$(RESET)"
+	echo "$(GREEN)[SUCCESS]: $@$(RESET)"
+
+$(OBJ_D)%.o : $(SRCS_D)%.c | $(OBJ_D)
+	echo "$(BLUE)[COMPILING]: $@$(RESET)"
+	$(CC) $(CFLAGS) -I/usr/include -Imlx_linux -O3 -c $< -o $@
+
+.PHONY: clean
+clean:
+	$(MAKE) clean -C ./libft
+	$(MAKE) clog
+	$(RM) $(OBJ_D)
+	echo "$(RED)[CLEAN]: obj files$(RESET)"
+
+.PHONY: fclean
+fclean: clean
+	$(MAKE) fclean -C ./libft
+	$(RM) $(NAME)
+	$(RM) $(BIN_D)
+	echo "$(RED)[CLEAN]: binaries$(RESET)"
+
+.PHONY: clog
+clog:
+	$(RM) $(LOG_D)
+	echo "$(RED)[CLEAN]: log$(RESET)"
+
+.PHONY: re
+re:
+	$(MAKE) fclean
+	$(MAKE) all
+
+# Create directories
+$(OBJ_D):
+	echo "$(YELLOW)[CREATING]: $(OBJ_D)$(RESET)"
+	mkdir -p $(OBJ_D)
+
+$(LOG_D):
+	echo "$(YELLOW)[CREATING]: $(LOG_D)$(RESET)"
+	mkdir -p $(LOG_D)
+
+$(BIN_D):
+	echo "$(YELLOW)[CREATING]: $(BIN_D)$(RESET)"
+	mkdir -p $(BIN_D)
+
+.PHONY: debug
+debug: all $(LOG_D)
+	$(BIN_D)$(NAME) $(ARGS) > $(LOG_D)$(shell date --iso=seconds).log
+	cat $(LOG_D)$(shell date --iso=seconds).log
+	echo "$(BLUE)[SAVED]: $(LOG_D)$(shell date --iso=seconds).log"
+
+.SILENT:
---
+++ src/main.c
@@ -0,0 +1,108 @@
+#include <mlx.h>
+#include <libft.h>
+#include <math.h>
+#include <complex.h>
+#include <stdio.h>
+
+typedef struct s_specs
+{
+	size_t	width;
+	size_t	height;
+	char *title;
+} t_specs;
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+
+void	set_pixel(t_data *data, int x, int y, int color)
+{
+	void	*dst;
+
+	// a annoter
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	return (t << 24 | r << 16 | g << 8 | b);
+}
+
+int	compute_color(int iteration, int max_iteration)
+{
+	int	r;
+	int	g;
+	int	b;
+	// int hue;
+
+	// hue = 255 * (iteration / max_iteration);
+
+	r = 255 * iteration / max_iteration;
+	g = 255 * iteration / max_iteration;
+	b = 100;
+	// r = (hue / 255.0) * 255;
+    // g = ((255 - hue) / 255.0) * 255;
+    // b = 128	;
+	return create_trgb(0, r, g, b);
+}
+
+int main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+	t_specs	specs;
+	int	threshold = 700;
+
+	specs.width = 720;
+	specs.height = 420;
+	specs.title = "HAAAAAA";
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, specs.width, specs.height, specs.title);
+
+	img.img = mlx_new_image(mlx, specs.width, specs.height);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+
+	float i = 0;
+	float j = 0;
+	size_t iteration = 0;
+	size_t max_iteration = 500;
+	float complex c;
+	float complex z;
+
+	while (j < specs.height)
+	{
+		i = 0;
+		while (i < specs.width)
+		{
+			c = (i / 360) + (j / 360) *I;
+			z = 0;
+			iteration = 0;
+			while (iteration < max_iteration)
+			{
+				if (cabsf(z) >= threshold)
+					break;
+				z = z*z + c;
+				iteration++;
+			}
+			printf("iteration: %zu abs(x): %f\n", iteration, cabsf(z));
+			// if (cabsf(z) >= 7832)
+			// 	set_pixel(&img, i, j, 0x00ff0000);
+			// else
+				set_pixel(&img, i, j, compute_color(iteration, max_iteration));
+			mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+			i++;
+		}
+		j++;
+	}
+	mlx_loop(mlx);
+
+
+}
